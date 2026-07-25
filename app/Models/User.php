60cdedf6cf2e33d65208e8d159f0734bfe2e0b45
Yusof Sadat Fakhr <yusofsadat@gmail.com>
2026-07-25 19:41:51 +0330

    Allow Telegram user creation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'telegram_chat_id', 'wallet_balance', 'is_admin'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = ['name', 'email', 'password', 'telegram_chat_id', 'wallet_balance', 'is_admin'];
+
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * Get the attributes that should be cast.
